25797db2068b68802f03c64ae2964d3df4558268
Thanh Huynh <huynhbathanh@gmail.com>
2021-01-16 00:25:02 +0700

    Add layouts homepage

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :listing_categories
   devise_for :users
 
-  root to: 'listings#index'
+  root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
