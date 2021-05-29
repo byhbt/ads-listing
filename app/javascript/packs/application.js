5ed8ff789fc17228f5a9fe481b45d99959f88c01
Thanh Huynh <huynhbathanh@gmail.com>
2021-05-29 16:43:39 +0700

    Replace generated project name

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,11 +13,9 @@ import 'regenerator-runtime/runtime';
 import 'initializers/';
 import 'screens/';
 
-import '../stylesheets/application';
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
-//
+
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
