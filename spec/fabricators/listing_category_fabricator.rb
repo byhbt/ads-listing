3c63318b018baddbc0d3a9d581e2a7d8758efc45
Thanh Huynh <huynhbathanh@gmail.com>
2021-05-29 10:14:12 +0700

    Remove predefine slug

--- spec/fabricators/listing_category_fabricator.rb
+++ spec/fabricators/listing_category_fabricator.rb
@@ -1,7 +1,6 @@
 Fabricator(:listing_category) do
   name_str = FFaker::Lorem.word.capitalize
   name { name_str }
-  slug { name_str.parameterize }
   description { FFaker::Lorem.paragraph[0..159] }
   display_order 1
 end
--- spec/fabricators/listing_fabricator.rb
+++ spec/fabricators/listing_fabricator.rb
@@ -1,7 +1,6 @@
 Fabricator(:listing) do
   title = FFaker::Lorem.word.capitalize
   title { title }
-  slug { title.parameterize }
   content { FFaker::Lorem.sentence }
   price { Random.new.rand(20000..10000000) }
   expired_at { Date.current + Random.new.rand(10..30).days }
