b44f38b2211343633306620bbe614c6f2e02f557
Thanh Huynh <huynhbathanh@gmail.com>
2021-05-30 17:15:54 +0700

    Change should to is_expected.to in rspec

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe Listing, type: :model do
   describe 'validations' do
-    it { should validate_presence_of(:title) }
-    it { should validate_presence_of(:content) }
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:content) }
   end
 end
